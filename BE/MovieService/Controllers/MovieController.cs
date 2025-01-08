a70f20afcc81082699142a93fca86c00aa4d591b
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-01-08 15:08:23 +0700

    update endpoint

--- BE/MovieService/Controllers/MovieController.cs
+++ BE/MovieService/Controllers/MovieController.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MovieService.Data;
-using SharedLibrary.Events;
-using MovieService.Models;
 using MovieService.Repositories;
 using SharedLibrary.EventBus;
 using MovieService.DTOs;
@@ -23,14 +21,14 @@ public class MovieController : ControllerBase
         _movieRepo = movieRepo;
     }
 
-    [HttpGet]
+    [HttpGet("public")]
     public async Task<IActionResult> GetAllMovies()
     {
         var movies = await _movieRepo.GetAllMoviesAsync();
         return Ok(movies);
     }
 
-    [HttpGet("{id}")]
+    [HttpGet("public/{id}")]
     public async Task<IActionResult> GetMovie(int id, [FromQuery] string userId)
     {
         var movie = await _movieRepo.GetMovieAsync(id, userId);
@@ -41,21 +39,21 @@ public class MovieController : ControllerBase
         return Ok(movie);
     }
 
-    [HttpPost]
+    [HttpPost("private")]
     public async Task<IActionResult> CreateMovie([FromBody] CreateMovieReq createMovieReq)
     {
         int id = await _movieRepo.AddMovieAsync(createMovieReq.MovieDto, createMovieReq.GenreIds, createMovieReq.ImageUrls, createMovieReq.StudioIds);
         return CreatedAtAction(nameof(GetMovie), new { id = id });
     }
 
-    [HttpPut("{id}")]
+    [HttpPut("private/{id}")]
     public async Task<IActionResult> UpdateMovie(int id, MovieDTO movie)
     {
         await _movieRepo.UpdateMovieAsync(id, movie);
         return NoContent();
     }
 
-    [HttpDelete("{id}")]
+    [HttpDelete("private/{id}")]
     public async Task<IActionResult> DeleteMovie(int id)
     {
         await _movieRepo.DeleteMovieAsync(id);
