eee70eb24deb37308789672ecef32aea8834642d
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-01-08 22:50:27 +0700

    add controller

---
+++ BE/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SearchService.Repositories;
+
+namespace SearchService.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ISearch _searchService;
+
+        public SearchController(ISearch searchService)
+        {
+            _searchService = searchService;
+        }
+
+        [HttpGet("public")]
+        public async Task<IActionResult> Search([FromQuery] string query)
+        {
+            var response = await _searchService.SearchMoviesAsync(query);
+            if (!response.IsValid)
+            {
+                return StatusCode(500, response.DebugInformation);
+            }
+
+            return Ok(response.Documents);
+        }
+    }
+}
