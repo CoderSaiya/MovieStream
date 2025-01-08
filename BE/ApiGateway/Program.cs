69afbe8d11a9ac8aa5df2aa536babbf573850a5e
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-01-08 14:58:08 +0700

    add authen JWT for api gateway

--- BE/ApiGateway/Program.cs
+++ BE/ApiGateway/Program.cs
@@ -1,10 +1,26 @@
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Load Ocelot configuration
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddAuthentication("JwtBearer")
+    .AddJwtBearer("JwtBearer", options =>
+    {
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = "your-issuer",
+            ValidAudience = "your-audience",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MovieFlix2025"))
+        };
+    });
 
 builder.Services.AddOcelot();
 
@@ -14,6 +30,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseOcelot().Wait();
 
