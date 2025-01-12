09d74f81d83a1e7689c32e53109e271a01422bb1
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-01-12 11:31:33 +0700

    update metadata

--- fe/src/app/(auth)/forgot-password/page.tsx
+++ fe/src/app/(auth)/forgot-password/page.tsx
@@ -2,8 +2,8 @@ import { Metadata } from "next";
 import { ForgotClient } from "./client";
 
 export const metadata: Metadata = {
-    title: "Sign In - MovieFlix",
-    description: "Sign in to your MovieFlix account",
+    title: "Forgot Password - MovieFlix",
+    description: "Easily reset your MovieFlix password to regain access to your favorite movies and TV shows. Simply follow the steps to create a new password and get back to streaming in no time.",
 };
 
 export default function ForgotPage() {
