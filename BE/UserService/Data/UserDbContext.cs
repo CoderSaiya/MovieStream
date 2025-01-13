63fbc1cbb41e4c4fa28ff13d559f3a2ef693c455
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-01-13 12:19:26 +0700

    update db context

--- BE/UserService/Data/UserDbContext.cs
+++ BE/UserService/Data/UserDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using UserService.Model;
 using UserService.Models;
 
 namespace UserService.Data;
@@ -8,10 +9,27 @@ public class UserDbContext : DbContext
     public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
 
     public DbSet<User> Users { get; set; }
+    public DbSet<UserProfile> UserProfile { get; set; }
+    public DbSet<UserLog> UserLog { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>().HasKey(u => u.Id);
+        modelBuilder.Entity<UserProfile>().HasKey(u => u.Id);
+        modelBuilder.Entity<UserLog>().HasKey(u => u.Id);
+
+        modelBuilder.Entity<User>()
+            .HasOne(u => u.UserProfile)
+            .WithOne(p => p.User)
+            .HasForeignKey<UserProfile>(p => p.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.UserLogs)
+            .WithOne(l => l.User)
+            .HasForeignKey(l => l.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
         base.OnModelCreating(modelBuilder);
     }
 }
