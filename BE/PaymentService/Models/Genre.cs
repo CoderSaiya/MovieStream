b5596290d080bfaa5c72c8f4f02b6263f368814e
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-01-17 12:23:25 +0700

    add models

---
+++ BE/PaymentService/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaymentService.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+    }
+}
---
+++ BE/PaymentService/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentService.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public int GenreId { get; set; }
+        [ForeignKey("GenreId")]
+        public Genre Genre { get; set; } = null!;
+        public double Amount { get; set; } = 0.0;
+        public string Description { get; set; } = null!;
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+    }
+}
