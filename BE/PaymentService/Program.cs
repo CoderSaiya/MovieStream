df2df1ee1f57a68eb7620f3055ae71454bb8ab67
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-01-17 19:18:08 +0700

    regist sql server

--- BE/PaymentService/Program.cs
+++ BE/PaymentService/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PaymentService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<PaymentDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"]);
+});
 
 var app = builder.Build();
 
