e3559f89c29ea531006f8a04c66a09e17f0c7734
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-01-17 16:03:49 +0700

    add controller

--- BE/notification-service/src/notification/notification.controller.ts
+++ BE/notification-service/src/notification/notification.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { NotificationService } from './notification.service';
+import { CreateNotificationDto } from './dto/create-notification';
 
 @Controller('notification')
-export class NotificationController {}
+export class NotificationController {
+  constructor(private readonly notificationService: NotificationService) {}
+
+  @Post()
+  async createNotification(@Body() data: CreateNotificationDto) {
+    return this.notificationService.handleNotificationEvent(data);
+  }
+}
