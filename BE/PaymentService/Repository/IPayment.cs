5b9cbe437d1bcea8e3a4eca6ed874335f764131e
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-01-17 12:49:13 +0700

    add service layer (repo)

---
+++ BE/PaymentService/Repository/IPayment.cs
@@ -0,0 +1,14 @@
+﻿using PaymentService.Models;
+
+namespace PaymentService.Repository
+{
+    public interface IPayment
+    {
+        Task CreateTractionAsync(Guid userId, double? amount);
+        Task CreateGenreAsync(string title);
+        Task<IEnumerable<Transaction>> GetAllTransactionsAsync();
+        Task<IEnumerable<Transaction>> GetTransactionByUserIdAsync(Guid userId);
+        Task<IEnumerable<Genre>> GetAllGenresAsync();
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ BE/PaymentService/Repository/PaymentRepo.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentService.Data;
+using PaymentService.Models;
+
+namespace PaymentService.Repository
+{
+    public class PaymentRepo : IPayment
+    {
+        private readonly PaymentDbContext _context;
+
+        public PaymentRepo(PaymentDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateGenreAsync(string title)
+        {
+            var genre = new Genre
+            {
+                Name = title,
+            };
+
+            await _context.Genre.AddAsync(genre);
+        }
+
+        public async Task CreateTractionAsync(Guid userId, double? amount)
+        {
+            var transaction = new Transaction
+            {
+                UserId = userId,
+                Amount = amount ?? 0.0
+            };
+
+            await _context.Transactions.AddAsync(transaction);
+        }
+
+        public async Task<IEnumerable<Genre>> GetAllGenresAsync()
+        {
+            return await _context.Genre.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Transaction>> GetAllTransactionsAsync()
+        {
+            return await _context.Transactions.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Transaction>> GetTransactionByUserIdAsync(Guid userId)
+        {
+            return await _context.Transactions
+                .Where(t => t.UserId == userId)
+                .Include(t => t.Genre)
+                .ToListAsync();
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
