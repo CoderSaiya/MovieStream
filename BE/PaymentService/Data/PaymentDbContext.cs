2d0b6a3dc4f60922a9dc0215f078cb2017d7e327
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-01-17 12:48:55 +0700

    add db context

---
+++ BE/PaymentService/Data/PaymentDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentService.Models;
+
+namespace PaymentService.Data
+{
+    public class PaymentDbContext : DbContext
+    {
+        public PaymentDbContext(DbContextOptions<PaymentDbContext> options) : base(options) { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Transaction>().HasKey(t  => t.Id);
+            modelBuilder.Entity<Genre>().HasKey(g => g.Id);
+
+            modelBuilder.Entity<Genre>()
+                .HasMany(t => t.Transactions)
+                .WithOne(t => t.Genre)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
