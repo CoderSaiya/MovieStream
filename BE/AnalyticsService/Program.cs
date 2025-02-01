d6ca0767c7d375f65625ba75e6554f9e1066dfcf
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-02-01 16:46:32 +0700

    config prometheus

--- BE/AnalyticsService/Program.cs
+++ BE/AnalyticsService/Program.cs
@@ -1,15 +1,27 @@
+using Prometheus;
+using Prometheus.SystemMetrics;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Regist System Metrics (CPU, RAM, Disk)
+builder.Services.AddSystemMetrics();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSingleton<ICollectorRegistry, CollectorRegistry>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseRouting();
+app.UseHttpMetrics();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapMetrics();
+});
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +29,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
